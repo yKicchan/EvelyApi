ce448303ca0204c55ceb71fe81c6e3a7a63ae59e
yKicchan <yKicchanApp@gmail.com>
2017-12-10 23:08:32 +0900

    add: ユーザーの詳細情報取得に対応

--- controller/users.go
+++ controller/users.go
@@ -5,7 +5,22 @@ import (
 	"EvelyApi/model"
 	"github.com/goadesign/goa"
 	mgo "gopkg.in/mgo.v2"
+	"log"
 )
+
+/**
+ * ユーザー情報をAPIのレスポンス形式に変換する
+ * @param  u ユーザー情報
+ * @return   レスポンス形式に変換したユーザー情報
+ */
+func ToUserMedia(u *model.UserModel) *app.User {
+	return &app.User{
+		ID:   u.ID,
+		Name: u.Name,
+		Mail: u.Mail,
+		Tel:  u.Tel,
+	}
+}
 
 // UsersController implements the users resource.
 type UsersController struct {
@@ -26,8 +41,11 @@ func (c *UsersController) Show(ctx *app.ShowUsersContext) error {
 	// UsersController_Show: start_implement
 
 	// Put your logic here
-
-	res := &app.User{}
-	return ctx.OK(res)
+	user, err := c.db.GetUser(ctx.UserID)
+	if err != nil {
+		log.Printf("[EvelyApi] %s", err)
+		return ctx.NotFound()
+	}
+	return ctx.OK(ToUserMedia(user))
 	// UsersController_Show: end_implement
 }
